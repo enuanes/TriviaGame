1b00cdb742dfd42e954f3b931d809932850a9733
Eric Nuanes <ecnuanes@outlook.com>
2018-03-01 00:45:11 -0800

    I updated the javascript file.

---
+++ assets/javascript/app.js
@@ -0,0 +1,331 @@
+var triviaQuestions = [{
+    question: "What is the capital of Alabama?",
+    answerList: ["Birmingham", "Montgomery","Huntsville", "Mobile"],
+    answer: 1
+},{
+    question: "What is the capital of Alaska?",
+    answerList: ["Anchorage", "Fairbanks", "Juneau","Sitka"],
+    answer: 2
+},{
+    question: "What is the capital of Arizona?",
+    answerList: ["Phoenix", "Tucson", "Mesa", "Chandler"],
+    answer: 0
+},{
+    question: "What is the capital of Arkansas?",
+    answerList: ["Little Rock", "Fort Smith", "Fayetteville", "Springdale"],
+    answer: 0
+},{
+    question: "What is the capital of California?",
+    answerList: ["Los Angeles", "San Diego", "San Francisco", "Sacramento"],
+    answer: 3
+},{
+    question: "What is the capital of Colorado?",
+    answerList: ["Denver", "Colorado Springs", "Aurora", "Fort Collins"],
+    answer: 0
+},{
+    question: "What is the capital of Connecticut?",
+    answerList: ["Bridgeport", "New Haven", "Stamford", "Hartford"],
+    answer: 3
+},{
+    question: "What is the capital of Delaware?",
+    answerList: ["Wilmington", "Dover", "Newark", "Middleton"],
+    answer: 1
+},{
+    question: "What is the capital of Florida?",
+    answerList: ["Jacksonville", "Miami", "Orlando", "Tallahassee"],
+    answer: 3
+},{
+    question: "What is the capital of Georgia?",
+    answerList: ["Atlanta", "Augusta", "Savannah", "Athens"],
+    answer: 0
+},{
+    question: "What is the capital Hawai'i?",
+    answerList: ["Pearl City", "Honolulu", "Hilo", "Kailua"],
+    answer: 1
+},{
+    question: "What is the capital of Idaho?",
+    answerList: ["Meridian", "Nampa", "Boise", "Idaho Falls"],
+    answer: 2
+},{
+    question: "What is the capital of Illinois?",
+    answerList: ["Joliet", "Rockford", "Chicago", "Springfield"],
+    answer: 3
+},{
+    question: "What is the capital of Indiana?",
+    answerList: ["Fort Wayne", "Indianapolis", "Evansville", "Southbend"],
+    answer: 1
+},{
+    question: "What is the capital of Iowa?",
+    answerList: ["Cedar Rapids", "Davenport", "Des Moines", "Sioux City"],
+    answer: 2
+},{
+    question: "What is the capital of Kansas?",
+    answerList: ["Wichita", "Kansas City", "Olathe", "Topeka"],
+    answer: 3
+},{
+    question: "What is the capital of Kentucky?",
+    answerList: ["Louisville", "Lexington", "Bowling Green", "Frankfort"],
+    answer: 3
+},{
+    question: "What is the capital of Louisiana?",
+    answerList: ["New Orleans", "Baton Rouge", "Shreveport", "Lafayette"],
+    answer: 1
+},{
+    question: "What is the capital of Maine?",
+    answerList: ["Portland", "Oxford", "Lewiston", "Augusta"],
+    answer: 3
+},{
+    question: "What is the capital of Maryland?",
+    answerList: ["Baltimore", "Silver Spring", "Annapolis", "Frederick"],
+    answer: 2
+},{
+    question: "What is the capital of Massachusetts?",
+    answerList: ["Boston", "Worcester", "Springfield", "Lowell"],
+    answer: 0
+},{
+    question: "What is the capital of Michigan?",
+    answerList: ["Detroit", "Grand Rapids", "Ann Arbor", "Lansing"],
+    answer: 3
+},{
+    question: "What is the capital of Minnesota?",
+    answerList: ["Minneapolis", "St. Paul", "Duluth", "Bloomington"],
+    answer: 1
+},{
+    question: "What is the capital of Mississippi?",
+    answerList: ["Jackson", "Gulfport", "Southaven", "Hattiesburg"],
+    answer: 0
+},{
+    question: "What is the capital of Missouri?",
+    answerList: ["Kansas City", "St. Louis", "Springfield", "Jefferson City"],
+    answer: 3
+},{
+    question: "What is the capital of Montana?",
+    answerList: ["Billings", "Missoula", "Helena", "Great Falls"],
+    answer: 2
+},{
+    question: "What is the capital of Nebraska?",
+    answerList: ["Omaha", "Lincoln", "Grand Island", "Kearney"],
+    answer: 1
+},{
+    question: "What is the capital of Nevada?",
+    answerList: ["Las Vegas", "Henderson", "Reno", "Carson City"],
+    answer: 3
+},{
+    question: "What is the capital of New Hampshire?",
+    answerList: ["Manchester", "Nashua", "Concord", "Derry"],
+    answer: 2
+},{
+    question: "What is the capital of New Jersey?",
+    answerList: ["Newark", "Jersey City", "Paterson", "Trenton"],
+    answer: 3
+},{
+    question: "What is the capital of New Mexico?",
+    answerList: ["Santa Fe", "Las Cruces", "Rio Rancho", "Albuquerque"],
+    answer: 0
+},{
+    question: "What is the capital of New York?",
+    answerList: ["New York City", "Buffalo", "Rochester", "Albany"],
+    answer: 3
+},{
+    question: "What is the capital of North Carolina?",
+    answerList: ["Charlotte", "Raleigh", "Greensboro", "Durham"],
+    answer: 1
+},{
+    question: "What is the capital of North Dakota?",
+    answerList: ["Fargo", "Bismarck", "Grand Forks", "Minot"],
+    answer: 1
+},{
+    question: "What is the capital of Ohio?",
+    answerList: ["Columbus", "Cleveland", "Cincinnati", "Toledo"],
+    answer: 0
+},{
+    question: "What is the capital of Oklahoma?",
+    answerList: ["Oklahoma City", "Tulsa", "Norman", "Borken Arrow"],
+    answer: 0
+},{
+    question: "What is the capital of Oregon?",
+    answerList: ["Portland", "Salem", "Eugene", "Gresham"],
+    answer: 1
+},{
+    question: "What is the capital of Pennsylvania?",
+    answerList: ["Philadelphia", "Pittsburgh", "Allentown", "Harrisburg"],
+    answer: 3
+},{
+    question: "What is the capital of Rhode Island?",
+    answerList: ["Warwick", "Cranston", "Providence", "Pawtucket"],
+    answer: 2
+},{
+    question: "What is the capital of South Carolina?",
+    answerList: ["Charleston", "Columbia", "Mount Pleasant", "Rock Hill"],
+    answer: 1
+},{
+    question: "What is the capital of South Dakota?",
+    answerList: ["Sioux Falls", "Rapid City", "Aberdeen", "Pierre"],
+    answer: 3
+},{
+    question: "What is the capital of Tennessee?",
+    answerList: ["Memphis", "Knoxville", "Nashville", "Chattanooga"],
+    answer: 2
+},{
+    question: "What is the capital of Texas?",
+    answerList: ["Houston", "San Antonio", "Dallas", "Austin"],
+    answer: 3
+},{
+    question: "What is the capital of Utah?",
+    answerList: ["West Valley City", "Salt Lake City", "Provo", "West Jordan"],
+    answer: 1
+},{
+    question: "What is the capital of Vermont?",
+    answerList: ["Burlington", "Essex", "Montpelier", "Rutland City"],
+    answer: 2
+},{
+    question: "What is the capital of Virginia?",
+    answerList: ["Virginia Beach", "Norfolk", "Chesapeake", "Richmond"],
+    answer: 3
+},{
+    question: "What is the capital of Washington?",
+    answerList: ["Seattle", "Olympia", "Spokane", "Tacoma"],
+    answer: 1
+},{
+    question: "What is the capital of West Virginia?",
+    answerList: ["Charleston", "Huntington", "Morgantown", "Parkersburg"],
+    answer: 0
+},{
+    question: "What is the capital of Wisconsin?",
+    answerList: ["Milwaukee", "Green Bay", "Madison", "Kenosha"],
+    answer: 2
+},{
+    question: "What is the capital of Wyoming?",
+    answerList: ["Casper", "Gillette", "Laramie", "Cheyenne"],
+    answer: 3
+}];
+
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13', 'question14', 'question15', 'question16', 'question17', 'question18', 'question19', 'question20', 'question21', 'question22', 'question23', 'question24', 'question25', 'question26', 'question27', 'question28', 'question29', 'question30', 'question31', 'question32', 'question33', 'question34', 'question35', 'question36', 'question37', 'question38', 'question39', 'question40', 'question41', 'question42', 'question43', 'question44', 'question45', 'question46', 'question47', 'question48', 'question49', 'question50'];
+var currentQuestion;
+var correctAnswer;
+var incorrectAnswer;
+var unanswered;
+var seconds;
+var time;
+var answered;
+var userSelect;
+var messages = {
+    correct: "Correct!",
+    incorrect: "Wrong!",
+    endTime: "Time's Up!",
+    finished: "Alright, let's see how well you did."
+}
+
+$('#startBtn').on('click', function() {
+    $(this).hide();
+    newGame();
+});
+
+$('#startOverBtn').on('click', function() {
+    $(this).hide();
+    newGame();
+});
+
+function newGame() {
+    $('#finalMessage').empty();
+    $('#correctAnswers').empty();
+    $('#incorrectAnswers').empty();
+    $('#unanswered').empty();
+    currentQuestion = 0;
+    correctAnswer = 0;
+    incorrectAnswer = 0;
+    unanswered = 0;
+    newQuestion();
+}
+
+function newQuestion() {
+    $('#message').empty();
+    $('#correctedAnswer').empty();
+    $('#gif').empty();
+    answered = true;
+
+    // This sets up new questions and answerList
+$('#currentQuestion').html('Question #' + (currentQuestion + 1) + '/' + triviaQuestions.length);
+$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+for(var i = 0; i < 4; i++) {
+    var choices = $('<div>');
+    choices.text(triviaQuestions[currentQuestion].answerList[i]);
+    choices.attr({'data-index': i});
+    choices.addClass('thisChoice');
+    $('.answerList').append(choices);
+}
+countdown();
+// Clicking an answer will pause the time and set up answerPage
+$('.thisChoice').on('click', function() {
+    userSelect = $(this).data('index');
+    clearInterval(time);
+    answerPage();
+});
+}
+
+function countdown() {
+    seconds = 15;
+    $('#timeLeft').html('<h3>Time Remianing: ' + seconds + '</h3>');
+    answered = true;
+    // This sets the time to go down
+    time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown() {
+    seconds--;
+    $('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+    if(seconds < 1) {
+        clearInterval(time);
+        answered = false;
+        answerPage();
+    }
+}
+
+function answerPage() {
+    $('#currentQuestion').empty();
+    $('.thisChoice').empty(); // This clears question page
+    $('.question').empty();
+
+    var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+    var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+    $('#gif').html('<img src = "assets/images/' + gifArray[currentQuestion] + '.gif" width = "400px">');
+
+    // This checks to see if correct, incorrect, or unanswered
+    if((userSelect == rightAnswerIndex) && (answered == true)) {
+        correctAnswer++;
+        $('#message').html(messages.correct);
+    } else if((userSelect != rightAnswerIndex) && (answered == true)) {
+        incorrectAnswer++;
+        $('#message').html(messages.incorrect);
+        $('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);        
+    }else{
+        unanswered++;
+        $('#message').html(messages.endTime);
+        $('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+        answered = true;
+    }
+
+    if(currentQuestion == (triviaQuestions.length-1)) {
+        setTimeout(scoreboard, 5000)
+    }else{
+        currentQuestion++;
+        setTimeout(newQuestion, 5000);
+    }
+}
+
+function scoreboard() {
+    $('#timeLeft').empty();
+    $('#message').empty();
+    $('#correctedAnswer').empty();
+    $('#gif').empty();
+    $('#finalMessage').html(messages.finished);
+    $('#correctAnswers').html("Correct Answers: " + correctAnswer);
+    $('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+    $('#unanswered').html("Unanswered: " + unanswered);
+    $('#startOverBtn').addClass('reset');
+    $('#startOverBtn').show();
+    $('#startOverBtn').html('Start Over?');
+}
+
+
+
